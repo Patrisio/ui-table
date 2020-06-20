ebc3b2e79586c4aae62d79dd4a7bcda770e1103b
Patrisio <45238380+Patrisio@users.noreply.github.com>
2020-06-20 23:49:11 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ui-table 
+    table-title="Table UI"
+  />
+</template>
+
+<script>
+
+import UiTable from './containers/UiTable/App.vue';
+
+export default {
+  components: { UiTable },
+}
+</script>
+
+<style>
+</style>
